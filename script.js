2081e3a1277c5d31ea363ca64e2161b6077bc71f
Emili Sophia Cordeiro <emilisophiassss@gmail.com>
2024-11-21 09:26:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-let vehicle1 = 'Qual o nome do primeiro veículo? '
-let speed1 = 'Qual a velocidade do primeiro veículo? '
-let vehicle2 = 'Qual a nome de segundo veículo?'
-let speed2 = 'Qual a velocidade do segundo veículo?'
+// Solicitar ao usuário o nome e a velocidade dos veículos
+let vehicle1 = prompt('Qual o nome do primeiro veículo?');
+let speed1 = parseInt(prompt('Qual a velocidade do primeiro veículo?'));
 
-if (speed1 === speed2){
-  console.log(`Tanto  ${vehicle1} quanto o ${vehicle2} estão na mesma velocidade de ${speed1}km/h`)
+let vehicle2 = prompt('Qual o nome do segundo veículo?');
+let speed2 = parseInt(prompt('Qual a velocidade do segundo veículo?'));
+
+// Comparar as velocidades e exibir o resultado
+if (speed1 === speed2) {
+  console.log(`Tanto o ${vehicle1} quanto o ${vehicle2} estão na mesma velocidade de ${speed1} km/h`);
 } else if (speed1 > speed2) {
-  console.log(`O ${vehicle1} está a ${speed1}km/h, e está mais rápido  que o ${vehicle2}, que está a ${speed2}km/h`)
+  console.log(`O ${vehicle1} está a ${speed1} km/h, e está mais rápido que o ${vehicle2}, que está a ${speed2} km/h`);
 } else {
-  console.log(`O ${vehicle2} está a ${speed2}km/h, e está mais rápido que o ${vehicle1}, que está a ${speed1}km/h`)
+  console.log(`O ${vehicle2} está a ${speed2} km/h, e está mais rápido que o ${vehicle1}, que está a ${speed1} km/h`);
 }
